d377cec66b5d9b30b6a24963cfcf139a6589a0ed
Chase J. Clark <chase_clark07@hotmail.com>
2019-10-13 14:38:41 -0400

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            Console.WriteLine("Enter server IP address:");
+            IPAddress ip = IPAddress.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter port number:");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type your message, followed by the enter key...");
+
+            //IPAddress ip = IPAddress.Parse("192.168.12.195");
+            while (true)
+            {
+                byte[] message = Encoding.ASCII.GetBytes(Console.ReadLine());
+                IPEndPoint ep = new IPEndPoint(ip, port);
+                s.SendTo(message, ep);
+                Console.WriteLine("Message sent to the broadcast address");
+            }
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    class Program
+    {
+        private const int port = 12345;
+        static void Main(string[] args)
+        {
+            UdpClient listener = new UdpClient(port);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
